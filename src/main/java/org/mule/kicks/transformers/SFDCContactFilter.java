bf28aba37692e662672880d767470851e57e4b36
Damian Sima <damiansima@gmail.com>
2013-12-04 11:22:35 -0300

    adding filter not to sync contact with country other than USA

--- src/main/java/org/mule/kicks/transformers/SFDCContactFilter.java
+++ src/main/java/org/mule/kicks/transformers/SFDCContactFilter.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.mule.api.MuleMessage;
 import org.mule.api.transformer.TransformerException;
@@ -17,6 +18,10 @@ import org.mule.transformer.AbstractMessageTransformer;
  */
 public class SFDCContactFilter extends AbstractMessageTransformer {
 	private static final String EMAIL_KEY = "Email";
+	private static final String MAILING_COUNTRY_LEY = "MailingCountry";
+
+	private static final String VALID_COUNTRY = "USA";
+
 	private static final String FILTERED_CONTACTS_COUNT = "filteredContactsCount";
 
 	private final static Logger logger = Logger.getLogger(SFDCContactFilter.class);
@@ -26,7 +31,9 @@ public class SFDCContactFilter extends AbstractMessageTransformer {
 
 		Map<String, String> contact = (Map<String, String>) message.getPayload();
 
-		if (contact.get(EMAIL_KEY) != null) {
+		System.out.println(contact.get("MailingCountry"));
+
+		if (StringUtils.isNotEmpty(contact.get(EMAIL_KEY)) && contact.get(MAILING_COUNTRY_LEY).equals(VALID_COUNTRY)) {
 			List<Map<String, String>> contactToSync = new ArrayList<Map<String, String>>();
 			contactToSync.add(contact);
 			message.setPayload(contactToSync);
