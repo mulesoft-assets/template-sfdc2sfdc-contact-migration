2f3d32854fc562903786b52892f1060e3210cc8a
Damian Sima <damiansima@gmail.com>
2014-05-28 13:16:06 -0300

    fixing date comparator adding date utils and its tests

--- src/main/java/org/mule/templates/util/ContactDateComparator.java
+++ src/main/java/org/mule/templates/util/ContactDateComparator.java
@@ -3,14 +3,14 @@ package org.mule.templates.util;
 import java.util.Map;
 
 import org.apache.commons.lang.Validate;
-import org.joda.time.DateTime;
-import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.DateTimeFormatter;
 
 /**
- * The function of this class is to establish a relation happens before between two maps representing SFDC contacts.
+ * The function of this class is to establish a relation happens before between
+ * two maps representing SFDC contacts.
  * 
- * It's assumed that these maps are well formed maps from SFDC thus they both contain an entry with the expected key. Never the less validations are being done.
+ * It's assumed that these maps are well formed maps from SFDC thus they both
+ * contain an entry with the expected key. Never the less validations are being
+ * done.
  * 
  * @author damiansima
  */
@@ -24,7 +24,8 @@ public class ContactDateComparator {
 	 *            SFDC contact map
 	 * @param contactB
 	 *            SFDC contact map
-	 * @return true if the last modified date from contactA is after the one from contact B
+	 * @return true if the last modified date from contactA is after the one
+	 *         from contact B
 	 */
 	public static boolean isAfter(Map<String, String> contactA, Map<String, String> contactB) {
 		Validate.notNull(contactA, "The contact A should not be null");
@@ -33,10 +34,6 @@ public class ContactDateComparator {
 		Validate.isTrue(contactA.containsKey(LAST_MODIFIED_DATE), "The contact A map should containt the key " + LAST_MODIFIED_DATE);
 		Validate.isTrue(contactB.containsKey(LAST_MODIFIED_DATE), "The contact B map should containt the key " + LAST_MODIFIED_DATE);
 
-		DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
-		DateTime lastModifiedDateOfA = formatter.parseDateTime(contactA.get(LAST_MODIFIED_DATE));
-		DateTime lastModifiedDateOfB = formatter.parseDateTime(contactB.get(LAST_MODIFIED_DATE));
-
-		return lastModifiedDateOfA.isAfter(lastModifiedDateOfB);
+		return DateUtils.isAfter(contactA.get(LAST_MODIFIED_DATE), contactB.get(LAST_MODIFIED_DATE));
 	}
 }
